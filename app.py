cf88b767cbca575e9b284bf145d86fc47b6ec675
ferkomes <ferkomes@users.noreply.github.com>
2019-04-10 21:43:30 +0200

    adding calculator and request

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+n = request.args.get("n")
 import os
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+FirstNumber = input()
+SecondNumber = input()
+FirstNumber=int(FirstNumber)
+SecondNumber=int(SecondNumber)
+
+print(FirstNumber+SecondNumber)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)), debug=True)
