8758648ea6a92570c8e823c5c7ce02056f46e83f
ferkomes <ferkomes@users.noreply.github.com>
2019-04-11 14:49:35 +0200

    sdf

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import sys
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def input_request():
     return render_template('input_request.html')
@@ -12,9 +11,8 @@ def input_request():
 
 @app.route('/', methods=['POST'])
 def result():
-    result = request.form
+    result = int(request.form['FirstNum']) + int(request.form['SecondNum'])
     return render_template("result.html", result=result)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)), debug=True)
