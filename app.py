7dbcc7857d289eb859d20470a241113f5739f03b
ferkomes <ferkomes@users.noreply.github.com>
2019-04-11 13:28:34 +0200

    first eclipse commit

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import sys
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def input_request():
     return render_template('input_request.html')
 
-@app.route('/',methods = ['POST'])
+
+@app.route('/', methods=['POST'])
 def result():
     result = request.form
-    return render_template("result.html",result = result)
+    return render_template("result.html", result=result)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)), debug=True)
